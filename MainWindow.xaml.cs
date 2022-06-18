890e4cffee0cf5c2a69038d7f67cb5820b9d18bb
Zheng JIANG <enzojz@gmail.com>
2022-06-18 02:33:56 +0200

    UI adjustments

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -45,15 +45,18 @@ namespace MonitorControl
         public DataTemplate Monitor { get; set; }
         public DataTemplate Profile { get; set; }
 
-        protected override DataTemplate SelectTemplateCore(object item)
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject dp)
         {
             if (item is Monitor)
             {
                 return Monitor;
             }
-            else
+            else if (item is String)
             {
                 return Profile;
+            } else
+            {
+                return null;
             }
         }
     }
