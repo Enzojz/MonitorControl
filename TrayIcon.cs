c74b942ea88428170ffb0e27350b837c74df9925
Zheng JIANG <enzojz@gmail.com>
2022-07-03 02:49:25 +0200

    Reseperate tray icon into native window

---
+++ TrayIcon.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorControl
+{
+    internal class TrayIcon : IDisposable
+    {
+        public TrayIcon()
+        {
+            var wndClassEx = new WinAPI.WNDCLASSEX
+            {
+                cbSize = Marshal.SizeOf(typeof(WinAPI.WNDCLASSEX)),
+                hInstance = Marshal.GetHINSTANCE(GetType().Module),
+                hCursor = IntPtr.Zero,
+                lpszMenuName = string.Empty,
+                lpszClassName = "MonitorControlTrayIcon",
+                lpfnWndProc = WindowProc
+            };
+
+            var atom = WinAPI.RegisterClassEx(ref wndClassEx);
+            if (0 == atom)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error(), "RegisterClassEx failed");
+            }
+
+            m_classAtom = new IntPtr(atom);
+
+            m_hWindow = WinAPI.CreateWindowEx(0, m_classAtom, IntPtr.Zero, 0, 0, 0, 0, 0, new IntPtr(HWND_MESSAGE), IntPtr.Zero, m_hInstance, IntPtr.Zero);
+            if (IntPtr.Zero == m_hWindow)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error(), "CreateWindowEx failed");
+            }
+        }
+
+        private IntPtr m_hInstance = IntPtr.Zero;
+        private IntPtr m_classAtom = IntPtr.Zero;
+        private IntPtr m_hWindow = IntPtr.Zero;
+
+        internal Action OpenWindow { get; set; }
+
+        internal Action<int, int> PopupMenu { get; set; }
+
+        public const int HWND_MESSAGE = -3;
+
+        protected virtual IntPtr WindowProc(IntPtr hWnd, WinAPI.WM msg, UIntPtr wParam, IntPtr lParam)
+        {
+            switch (msg)
+            {
+                case WinAPI.WM.WM_CREATE:
+                    SetTrayIcon(hWnd);
+                    break;
+                case WinAPI.WM.WM_DESTROY:
+                    RemoveTrayIcon(hWnd);
+                    break;
+                case WinAPI.WM.WM_USER:
+                    switch ((WinAPI.WM)lParam)
+                    {
+                        case WinAPI.WM.WM_RBUTTONDOWN:
+                            if (PopupMenu != null)
+                            {
+                                WinAPI.Point pt;
+                                WinAPI.GetCursorPos(out pt);
+                                PopupMenu(pt.x, pt.y);
+                            }
+                            break;
+                        case WinAPI.WM.WM_LBUTTONDOWN:
+                            if (OpenWindow != null)
+                                OpenWindow();
+                            break;
+                    }
+                    return IntPtr.Zero;
+            }
+            return WinAPI.DefWindowProcW(hWnd, (uint)msg, wParam, lParam);
+        }
+
+        private void SetTrayIcon(IntPtr hWnd)
+        {
+
+            var data = new WinAPI.NOTIFYICONDATA
+            {
+                cbSize = Marshal.SizeOf<WinAPI.NOTIFYICONDATA>(),
+                hWnd = hWnd,
+                uFlags = WinAPI.NotifyFlags.NIF_ICON | WinAPI.NotifyFlags.NIF_MESSAGE,
+                uID = 1,
+                hIcon = WinAPI.LoadImage(IntPtr.Zero, "Assets/MonitorControl.ico", 1, 32, 32, 0x00000010),
+                uCallbackMessage = WinAPI.WM.WM_USER,
+            };
+
+            WinAPI.Shell_NotifyIcon(WinAPI.NotifyIconMessage.NIM_ADD, ref data);
+        }
+
+        private void RemoveTrayIcon(IntPtr hWnd)
+        {
+            var data = new WinAPI.NOTIFYICONDATA
+            {
+                cbSize = Marshal.SizeOf<WinAPI.NOTIFYICONDATA>(),
+                hWnd = hWnd,
+                uID = 1
+            };
+
+            WinAPI.Shell_NotifyIcon(WinAPI.NotifyIconMessage.NIM_DELETE, ref data);
+        }
+
+        public void Dispose()
+        {
+            if (m_hWindow != IntPtr.Zero)
+            {
+                try
+                {
+                    WinAPI.DestroyWindow(this.m_hWindow);
+                }
+                catch (System.ExecutionEngineException e)
+                {
+
+                }
+                finally
+                {
+                    m_hWindow = IntPtr.Zero;
+                }
+            }
+
+            if (m_classAtom != IntPtr.Zero)
+            {
+                WinAPI.UnregisterClass(m_classAtom, m_hInstance);
+            }
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
---
+++ Views/PopupMenu.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MonitorControl
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PopupMenu : Window
+    {
+        const int GWL_STYLE = -16;
+        public PopupMenu()
+        {
+            this.InitializeComponent();
+            m_hWindow = WinRT.Interop.WindowNative.GetWindowHandle(this);
+
+            var hIcon = WinAPI.LoadImage(IntPtr.Zero, "Assets/MonitorControl.ico", 1, 32, 32, 0x00000010);
+            WinAPI.SendMessage(m_hWindow, 0x0080, 0, hIcon);
+
+            var idWindow = Win32Interop.GetWindowIdFromWindow(m_hWindow);
+            var appWindow = AppWindow.GetFromWindowId(idWindow);
+
+            var presenter = appWindow.Presenter as OverlappedPresenter;
+            presenter.IsResizable = false;
+            presenter.SetBorderAndTitleBar(false, false);
+
+            m_backdropHelper = new BackdropManager(this);
+            App.SettingManager.ThemeChanged += ThemeChanged;
+            ThemeChanged(null, App.SettingManager.ThemeEnum);
+        }
+
+        private IntPtr m_hWindow;
+        private Rect m_rect;
+        private BackdropManager m_backdropHelper;
+
+        internal Action OpenWindow;
+        internal Action ExitApplication;
+
+        internal void SetPosition(int x, int y)
+        {
+            ItemList.ItemsSource = MenuItems;
+            float dpi = WinAPI.GetDpiForWindow(m_hWindow);
+            var dpiScaling = dpi / 96;
+
+            m_rect.Width = 200 * dpiScaling;
+            m_rect.Height = (App.Instance.Profiles.Count * 40 + 84) * dpiScaling;
+            m_rect.X = x - m_rect.Width;
+            m_rect.Y = y - m_rect.Height;
+
+            WinAPI.SetWindowPos(m_hWindow, -1, (int)m_rect.X, (int)m_rect.Y, (int)m_rect.Width, (int)m_rect.Height, 0x0040);
+        }
+
+        internal void Hide()
+        {
+            WinAPI.SetWindowPos(m_hWindow, 1, (int)m_rect.X, (int)m_rect.Y, (int)m_rect.Width, (int)m_rect.Height, 0x0080);
+        }
+        private void ThemeChanged(object sender, BackdropManager.BackdropType backdrop)
+        {
+            m_backdropHelper.SetBackdrop(backdrop);
+        }
+
+        public PopupMenuItem[] MenuItems
+        {
+            get
+            {
+                return App.Instance.Profiles
+                    .Select(profile => new PopupMenuItem()
+                    {
+                        Text = profile.Name,
+                        IsChecked = App.Instance.CurrentProfile != null && App.Instance.CurrentProfile.Profile.Guid == profile.Profile.Guid,
+                        Callback = () => {
+                            Hide();
+                            App.Instance.LoadProfile(profile.Name);
+                        }
+                    })
+                    .Union(
+                    new PopupMenuItem[]
+                    {
+                        new PopupMenuItem(),
+                        new PopupMenuItem() {
+                            Text = "Show Monitor Control",
+                            Callback = () => {
+                                Hide();
+                                OpenWindow();
+                            }
+                        },
+                        new PopupMenuItem() {
+                            Text = "Exit",
+                            Callback = () => {
+                                ExitApplication();
+                            }
+                        }
+                    }
+                    ).ToArray();
+            }
+        }
+    }
+}
